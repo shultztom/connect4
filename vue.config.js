d9ea150ff9e92f26f7c3f5afe2ee153a33567de8
shultztom <shultzthomask@sau.edu>
2021-03-07 22:12:26 -0600

    gh-deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  ublicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/connect4'
     : '/'
 }
